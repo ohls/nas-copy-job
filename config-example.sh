4056da2f6c89c30eaf16d56044637d8ada13bc52
Sönke Ohls <S@Ohls.eu>
2024-01-03 19:43:19 +0100

    Move photos from (sync representation of mobile phone on NAS) Internal-Memory to a yearly sub-folder in SD-Card. Beautify output

--- config-example.sh
+++ config-example.sh
@@ -1,16 +1,49 @@
-# Rename this file to config.sh then:
+# Rename this file to config.sh
+
+# ======================================================================================================================
 # Enter all your personal configuration and make sure to not add it to your git repo
 #   this way you can have different configurations on your test folder and your real job folder
 #   and you keep your personal data out of your (public) repo
-#
-# Change DRYRUN to false to get files moved, everything else is just a dry-run
+# ======================================================================================================================
+
+
+# General --------------------------------------------------------------------------------------------------------------
+# Do a dry-run without moving files around, boolean true/false (no quotes)
 DRYRUN=true
-DAYS_WHEN_PHOTO_IS_OLD=12
+
+
+
+# Path for automated photo copies. -------------------------------------------------------------------------------------
+#   Recommended to use a "Team Folder" of Synology Drive to sync with multiple user PCs
+
+# Automated copy of photos will be done of photos that are newer than DAYS_WHEN_PHOTO_IS_OLD in days
+DAYS_WHEN_PHOTO_IS_OLD=400
+
+#   Path to the source folder to generate automated copies of photos (no trailing slash/)
+PHOTO_SOURCE_PATH="/volume1/Test/Source/Photos"
+
+#   Path to the target folder to receive automated copies of photos (no trailing slash/)
+PHOTO_TARGET_PATH="/volume1/Test/Target/Automated-Copies/photos-recent-of-months"
+
+
+
+# Sort photos from Internal Memory to SD-Card --------------------------------------------------------------------------
+
+# Only photos older than DAYS_TO_DELAY_MOBILE_PHOTOS days will be moved from Internal Memory to SD-Card
 DAYS_TO_DELAY_MOBILE_PHOTOS=14
+
+# List of usernames where photos shall be moved from Internal-Memory to SD-Card
 USERS=("Tim" "Jill")
 
-PHOTO_SOURCE_PATH="/volume1/Public/Temp/copy-job-test/source"
-PHOTO_TARGET_PATH="/volume1/Public/Temp/copy-job-test/target"
+# Path to the root of the homes folder of Synology NAS (no trailing slash/)
+HOMES_PATH="/volume1/Test/homes"
 
-INTERNAL_MEMORY_DCIM_CAMERA_ON_NAS="/volume1/Test/copy-job-test/Internal-Memory/DCIM/Camera"
-SD_CARD_DCIM_CAMERA_ON_NAS="/volume1/Test/copy-job-test/SD-Card/DCIM/Camera"
+# Folder inside home of user that represents the sync of photos of the users smart-phone
+#   no leading /slash, no trailing slash/
+#   this is the part of the path that follows HOMES_PATH/USERNAME
+
+# Internal Memory Folder (not path), synced folder of smart phone on NAS
+INTERNAL_MEMORY_DCIM_CAMERA_ON_NAS_FOLDER="Handy-Sync/Internal-Memory/DCIM/Camera"
+
+# SD-Card Folder (not path), synced folder of smart phone on NAS
+SD_CARD_DCIM_CAMERA_ON_NAS_FOLDER="Handy-Sync/SD-Card/DCIM/Camera"
--- nas-copy-photos-of-last-months.sh
+++ nas-copy-photos-of-last-months.sh
@@ -14,57 +14,130 @@
 
 
 # Code -----------------------------------------------------------------------------------------------------------------
-echo "***********************************************************************"
-echo "***                          NAS Copy Job                           ***"
-echo "***********************************************************************"
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+echo "*****************************************************************************************************************"
+echo "***                                      NAS Copy Job                                                         ***"
+echo "*****************************************************************************************************************"
 echo "*"
 echo "*   Run time: $(date) @ $(hostname)"
 echo "*"
-echo "*"
+
+if ! test -f "./config.sh"; then
+    echo "*"
+    echo "*    ERROR: No config file found in '$SCRIPT_DIR'"
+    echo "*    ----------------------------------------------------"
+    echo "*"
+    echo "*       Rename file 'config-example.sh' to 'config.sh'"
+    echo "*       Make sure all configurations are set properly."
+    echo "*       Poor settings may cause data loss!"
+    echo "*"
+    echo "*       Download latest script version and"
+    echo "*       its 'config-example.sh' file from"
+    echo "*       https://github.com/ohls/nas-copy-job"
+    echo "*  "
+    echo "*  "
+
+    exit 1
+    #^^^^^
+fi
+
 echo "*   PHOTO_SOURCE_PATH: $PHOTO_SOURCE_PATH"
 echo "*   PHOTO_TARGET_PATH: $PHOTO_TARGET_PATH"
 echo "*   DRYRUN: $DRYRUN"
-echo ""
-echo ""
-# safely copy all JPEGs from PHOTO_SOURCE_PATH to PHOTO_TARGET_PATH
+echo "*"
+echo "*"
+echo "*"
+
+
+echo "***  Make automated copy of recent photos ***********************************************************************"
+echo "*****************************************************************************************************************"
+# safely copy all JPEGs from PHOTO_SOURCE_PATH to PHOTO_TARGET_PATH ----------------------------------------------------
 #    if is newer than DAYS_WHEN_PHOTO_TARGET_PATH_IS_OLD days.
 #    delete files in the PHOTO_TARGET_PATH that are not in the PHOTO_SOURCE_PATH; delete ghosts
 #    Debug info: --dry-run (n) in -axprvn # -v is verbose (a lot of output)
 if [ "$DRYRUN" = true ] ; then
   rsync_options="-axprvn --delete"
-  echo "*** Begin rsync copy photos with flags $rsync_options ***"
 else
   rsync_options="-axpr --delete"
-  echo "*** Begin rsync copy photos with flags $rsync_options ***"
 fi
 
+echo "*** Copy photos with flags $rsync_options"
 find "$PHOTO_SOURCE_PATH" -type f \( -name '*.jpg' -o -name '*.JPG' \) -mtime -"$DAYS_WHEN_PHOTO_IS_OLD" -printf %P\\0| eval rsync "$rsync_options" --files-from=- --from0 "$PHOTO_SOURCE_PATH $PHOTO_TARGET_PATH"
 echo ""
 
 # delete files in PHOTO_TARGET_PATH that are older than DAYS_WHEN_PHOTO_TARGET_PATH_IS_OLD days
 # ^^^^^^^^^^^^   this is tested, but dangerous
 if [ "$DRYRUN" = true ] ; then
-  echo "*** Dry-run: Delete photos in PHOTO_TARGET_PATH older than $DAYS_WHEN_PHOTO_IS_OLD days ***"
+  echo "*** Dry-run: Delete photos in PHOTO_TARGET_PATH older than $DAYS_WHEN_PHOTO_IS_OLD days"
 else
-  echo "*** Delete photos in PHOTO_TARGET_PATH older than $DAYS_WHEN_PHOTO_IS_OLD days ***"
+  echo "*** Delete photos in PHOTO_TARGET_PATH older than $DAYS_WHEN_PHOTO_IS_OLD days"
   find "$PHOTO_TARGET_PATH" -type f -mtime +"$DAYS_WHEN_PHOTO_IS_OLD" -exec rm -f {} \;
 fi
 echo ""
 
+echo "***  Move and sort smart-phone photos  **************************************************************************"
+echo "*****************************************************************************************************************"
+# Sort and clean Smart Phone Camera photos -----------------------------------------------------------------------------
+#   On the NAS folder that represent the synced/uploaded data from smart-phone
+#   take all photos taken from camera, stored on Internal Memory and
+#   after a few days delay to allow user clean up of "ugly" snap shots (set DAYS_TO_DELAY_MOBILE_PHOTOS)
+#   move the photos to the SD-Card (representation on NAS) to free up expensive Internal Memory
+#   into a yearly sub-folder.
+#   Nothing will be done on the smart phone, only on the NAS (recommended at night)
+#   Synology Drive can then sync the NAS folder with the smart-phone to actually do the operation on the smart-phone
 
 YEAR_NOW=$(date +"%Y")
 YEARS_AGO=$((YEAR_NOW-5))
-echo "This year is $YEAR_NOW"
-echo "Years ago was $YEARS_AGO"
+YEAR_LAST=$(date --date="$DAYS_TO_DELAY_MOBILE_PHOTOS days ago" '+%Y')
+
+# echo "This year is $YEAR_NOW"
+# echo "Years ago was $YEARS_AGO"
+
+if [ "$DRYRUN" = true ] ; then
+  rsync_options="-axprvn --remove-source-files"
+else
+  # do not use --delete option here, because it is a move it may create data loss
+  rsync_options="-axpr --remove-source-files"
+fi
+
 
 for user in ${USERS[@]}; do
-  echo "* Sort $user's photos from Internal Memory to SD-Card (in its sync on NAS)"
+  echo "* Sort $user's photos from Internal Memory to SD-Card into a yearly sub-folder (on its sync on NAS)"
+  echo "*   rsync options $rsync_options"
 
-    for (( y=YEARS_AGO; y<=YEAR_NOW; y++ ))
-    do
-      echo "*   Begin rsync move photos of $y"
-      echo "*     from $user/$INTERNAL_MEMORY_DCIM_CAMERA_ON_NAS"
-      echo "*     to   $user/$SD_CARD_DCIM_CAMERA_ON_NAS/$y"
-    done
+  for (( y=YEARS_AGO; y<=YEAR_LAST; y++ ))
+  do
+    source_path="$HOMES_PATH/$user/$INTERNAL_MEMORY_DCIM_CAMERA_ON_NAS_FOLDER"
+    target_path="$HOMES_PATH/$user/$SD_CARD_DCIM_CAMERA_ON_NAS_FOLDER/$y"
+    first_day_in_year="$y-01-01 00:00:00"
+    last_day_in_year="$y-12-31 23:59:59"
+    if [ "$y" -eq "$YEAR_LAST" ]; then
+      last_day_in_year=$(date --date="$DAYS_TO_DELAY_MOBILE_PHOTOS days ago" '+%Y-%m-%d 23:59:59')
+    fi
+    echo "*"
+    echo "*   $y: Move photos modified between $first_day_in_year and $last_day_in_year"
+    echo "*     from $source_path"
+    echo "*     to   $target_path"
+
+    # important to create the target folder if it doesn't exist.
+    #   Otherwise rsync will take it as target file name and move all photos to the same file
+    #   overwriting them and only the last photo remains with year as filename.
+    #   DANGEROUS!!! In this case all photos (but the last per year) get deleted
+    mkdir -p $target_path
+
+    find "$source_path" -type f \( -name '*.jpg' -o -name '*.JPG' \) -newermt "$first_day_in_year" ! -newermt "$last_day_in_year" -printf %P\\0| eval rsync "$rsync_options" --files-from=- --from0 "$source_path $target_path"
+
+  done
+
+  echo "*"
+  echo "*"
 
 done
+
+echo "*"
+echo "*"
+echo "*"
+echo "*   Finished at run time: $(date)"
+echo "*                                                                                                               *"
+echo "*****************************************************************************************************************"
+
